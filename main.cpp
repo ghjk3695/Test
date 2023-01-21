8d30f89382b89d935619e37a380de06e8a6dcdd1
Yiyang Lu <122488816+ste900811@users.noreply.github.com>
2023-01-20 23:28:10 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+
+void mergesort(int ar[]){
+    if (sizeof(ar)/sizeof(ar[0]) == 1){
+        return;
+    }
+    
+    {
+        /* code */
+    }
+    
+}
+
+
+int main(){
+    int arr[8] = {3,5,4,6,1,2,7,0};
+    return 0;
+}
